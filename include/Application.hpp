dc9b62ba31356bc360367e37d1d5ec5ffe44eb71
Nick <nickphilomath@gmail.com>
2025-03-28 21:51:04 +0500

    did any shit

--- include/Application.hpp
+++ include/Application.hpp
@@ -18,14 +18,30 @@ public:
     void run();
 
 private:
+    void                        createDynamicsWorld();
+    btBoxShape*                 createBoxShape(const btVector3& halfExtents);
+    btRigidBody*                createRigidBody(float mass, const btTransform& startTransform, btCollisionShape* shape, const btVector4& color);
+
     btRigidBody*                createCube(float x, float y, float z, float size, float mass);
     void                        drawCube(btRigidBody* body);
 
-    btDiscreteDynamicsWorld*    m_world = nullptr;
     std::vector<btRigidBody*>   m_rigidBodies;
     Camera3D                    m_camera = { 0 };
 
+    //keep the collision shapes, for deletion/cleanup
+	btAlignedObjectArray<btCollisionShape*> m_collisionShapes;
+	btBroadphaseInterface* m_broadphase;
+	btCollisionDispatcher* m_dispatcher;
+	btConstraintSolver* m_solver;
+	btDefaultCollisionConfiguration* m_collisionConfiguration;
+	btDiscreteDynamicsWorld* m_dynamicsWorld;
+
     btRigidBody*                m_ground = nullptr;
+    Model                       m_groundModel;
+    Vector3                     m_groundSize = { 80, 1, 80 };
+    Vector3                     m_groundPos = { 0, 0, 0 };
     Model                       m_cubeModel;
+
     Texture2D                   m_texture_dark;
+    Texture2D                   m_texture_orange;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,29 +1,15 @@
 #include "Application.hpp"
 
+#define ARRAY_SIZE_Y 5
+#define ARRAY_SIZE_X 5
+#define ARRAY_SIZE_Z 5
+
 Application::Application() {
     // Initialize Raylib
     const int screenWidth = 1366;
     const int screenHeight = 768;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "Bullet Physics with Raylib");
-    
-    // Set up Bullet Physics world
-    btDefaultCollisionConfiguration* collisionConfig = new btDefaultCollisionConfiguration();
-    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfig);
-    btBroadphaseInterface* broadphase = new btDbvtBroadphase();
-    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver();
-    m_world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
-    m_world->setGravity(btVector3(0, -9.81f, 0));
-
-    // Create ground
-    const Vector3 groundSize = { 20, 1, 20 };
-    const Vector3 groundPos = { 0, 0, 0 };
-
-    btCollisionShape* groundShape = new btBoxShape(btVector3(groundSize.x / 2, groundSize.y / 2, groundSize.z / 2));
-    btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(groundPos.x, groundPos.y, groundPos.z)));
-    
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-    m_ground = new btRigidBody(groundRigidBodyCI);
-    m_world->addRigidBody(m_ground);
 
     // Set up Camera
     m_camera.position = (Vector3){ 10.0f, 10.0f, 15.0f };
@@ -32,34 +18,137 @@ Application::Application() {
     m_camera.fovy = 45.0f;
     m_camera.projection = CAMERA_PERSPECTIVE;
 
-    // Load Cube Model
-    m_cubeModel = LoadModel("resources/models/cube.obj");
     // load textures
-    m_texture_dark = LoadTexture("resources/textures/kenney_prototype-textures/PNG/Orange/texture_08.png");
-    m_cubeModel.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = m_texture_dark;
+    m_texture_orange = LoadTexture("resources/textures/kenney_prototype-textures/PNG/Orange/texture_08.png");
+    m_texture_dark   = LoadTexture("resources/textures/kenney_prototype-textures/PNG/Dark/texture_07.png");
+    SetTextureWrap(m_texture_dark, TEXTURE_WRAP_REPEAT);
+    
+    // Set up Bullet Physics world
+    createDynamicsWorld();
+
+    // Create ground
+    // btCollisionShape* groundShape = new btBoxShape(btVector3(m_groundSize.x / 2, m_groundSize.y / 2, m_groundSize.z / 2));
+    // btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(m_groundPos.x, m_groundPos.y, m_groundPos.z)));
+    
+    // btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
+    // m_ground = new btRigidBody(groundRigidBodyCI);
+    // m_dynamicsWorld->addRigidBody(m_ground);
+
+    m_groundModel = LoadModelFromMesh(GenMeshCube(m_groundSize.x, m_groundSize.y, m_groundSize.z));
+    m_groundModel.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = m_texture_dark;
+
+    btBoxShape* groundShape = createBoxShape(btVector3(m_groundSize.x / 2, m_groundSize.y / 2, m_groundSize.z / 2));
+    m_collisionShapes.push_back(groundShape);
+
+    btTransform groundTransform;
+	groundTransform.setIdentity();
+	groundTransform.setOrigin(btVector3(0, 0, 0));
+
+    {
+		btScalar mass(0.);
+		createRigidBody(mass, groundTransform, groundShape, btVector4(0, 0, 1, 1));
+	}
+
+    {
+		//create a few dynamic rigidbodies
+		// Re-using the same collision is better for memory usage and performance
+
+		btBoxShape* colShape = createBoxShape(btVector3(0.5, 0.5, 0.5));
+
+		//btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+		m_collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		// btVector3 localInertia(0, 0, 0);
+		// if (isDynamic)
+		// 	colShape->calculateLocalInertia(mass, localInertia);
+
+		for (int k = 0; k < ARRAY_SIZE_Y; k++)
+		{
+			for (int i = 0; i < ARRAY_SIZE_X; i++)
+			{
+				for (int j = 0; j < ARRAY_SIZE_Z; j++)
+				{
+					startTransform.setOrigin(btVector3(
+						btScalar(1 * i),
+						btScalar(10 + 1 * k),
+						btScalar(1 * j)));
+
+                    createRigidBody(mass, startTransform, colShape, btVector4(1, 0, 0, 1));
+				}
+			}
+		}
+	}
+
+
+    // Load Cube Model
+    // m_cubeModel = LoadModel("resources/models/cube.obj");
+    m_cubeModel = LoadModelFromMesh(GenMeshCube(1.0f, 1.0f, 1.0f));
+    m_cubeModel.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = m_texture_orange;
 }
 
 Application::~Application() {
     // Cleanup Bullet Physics
     for (btRigidBody* body : m_rigidBodies) {
-        m_world->removeRigidBody(body);
+        m_dynamicsWorld->removeRigidBody(body);
         delete body->getMotionState();
         delete body->getCollisionShape();
         delete body;
     }
-    m_world->removeRigidBody(m_ground);
-    delete m_ground->getMotionState();
-    delete m_ground->getCollisionShape();
-    delete m_ground;
     
-    delete m_world;
-    // delete solver;
-    // delete broadphase;
-    // delete dispatcher;
-    // delete collisionConfig;
+    delete m_dynamicsWorld;
+    delete m_solver;
+    delete m_broadphase;
+    delete m_dispatcher;
+    delete m_collisionConfiguration;
 
     // Close Raylib
     CloseWindow();
+}
+
+void Application::createDynamicsWorld()
+{
+    m_collisionConfiguration = new btDefaultCollisionConfiguration();
+    m_dispatcher = new btCollisionDispatcher(m_collisionConfiguration);
+    m_broadphase = new btDbvtBroadphase();
+    m_solver = new btSequentialImpulseConstraintSolver();
+    m_dynamicsWorld = new btDiscreteDynamicsWorld(m_dispatcher, m_broadphase, m_solver, m_collisionConfiguration);
+    m_dynamicsWorld->setGravity(btVector3(0, -9.81f, 0));
+}
+
+btBoxShape* Application::createBoxShape(const btVector3& halfExtents)
+{
+    btBoxShape* box = new btBoxShape(halfExtents);
+    return box;
+}
+
+btRigidBody *Application::createRigidBody(float mass, const btTransform &startTransform, btCollisionShape *shape, const btVector4 &color = btVector4(1, 0, 0, 1))
+{
+    btAssert((!shape || shape->getShapeType() != INVALID_SHAPE_PROXYTYPE));
+
+    //rigidbody is dynamic if and only if mass is non zero, otherwise static
+    bool isDynamic = (mass != 0.f);
+
+    btVector3 localInertia(0, 0, 0);
+    if (isDynamic)
+        shape->calculateLocalInertia(mass, localInertia);
+
+    //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+    btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+    btRigidBody::btRigidBodyConstructionInfo cInfo(mass, myMotionState, shape, localInertia);
+    btRigidBody* body = new btRigidBody(cInfo);
+
+    body->setUserIndex(-1);
+    m_dynamicsWorld->addRigidBody(body);
+    return body;
 }
 
 btRigidBody *Application::createCube(float x, float y, float z, float size, float mass)
@@ -75,7 +164,7 @@ btRigidBody *Application::createCube(float x, float y, float z, float size, floa
     btRigidBody::btRigidBodyConstructionInfo cubeRigidBodyCI(mass, cubeMotionState, cubeShape, cubeInertia);
     btRigidBody* cubeRigidBody = new btRigidBody(cubeRigidBodyCI);
 
-    m_world->addRigidBody(cubeRigidBody);
+    m_dynamicsWorld->addRigidBody(cubeRigidBody);
     m_rigidBodies.push_back(cubeRigidBody);
     return cubeRigidBody;
 }
@@ -101,63 +190,23 @@ void Application::drawCube(btRigidBody *cube)
 
     // Draw the transformed model
     DrawModel(m_cubeModel, {0, 0, 0}, 1.0f, WHITE);
-    // DrawModelWires(m_cubeModel, {0, 0, 0}, 1.0f, RED);
+    DrawModelWires(m_cubeModel, {0, 0, 0}, 1.0f, RED);
 }
 
 void Application::run() {
-    // SetTargetFPS(60);
+    SetTargetFPS(60);
     DisableCursor();
-
-    int count = 0;
-
-    // create cubes
-    createCube(0, 10, 0, 1, 1);
-    count++;
-    bool spawnStopped = false;
-
-    btCollisionShape* ballShape = new btSphereShape(3);
-    btDefaultMotionState* ballMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 5, 0)));
-    btRigidBody::btRigidBodyConstructionInfo ballRigidBodyCI(1, ballMotionState, ballShape, btVector3(0, 0, 0));
-    btRigidBody* ballRigidBody = new btRigidBody(ballRigidBodyCI);
-    m_world->addRigidBody(ballRigidBody);
 
 
     while (!WindowShouldClose()) {
         // Update
         UpdateCamera(&m_camera, CAMERA_FREE);
 
-        Vector3 cameraPos = { m_camera.position.x, m_camera.position.y, m_camera.position.z };
-        btTransform ballTransform;
-        ballTransform.setIdentity();
-        ballTransform.setOrigin(btVector3(cameraPos.x, cameraPos.y, cameraPos.z));
-        ballRigidBody->setWorldTransform(ballTransform);
-        ballRigidBody->getMotionState()->setWorldTransform(ballTransform);
+        if (IsKeyDown(KEY_LEFT_CONTROL)) EnableCursor();
+        if (IsKeyReleased(KEY_LEFT_CONTROL)) DisableCursor();
 
-        if (IsKeyPressed('R')) {spawnStopped = false; count = 0;}
-        if (count < 300 && !spawnStopped) {
-            createCube(0, 20, 0, 1, 1);
-            count++;
-        } else spawnStopped = true;
+        m_dynamicsWorld->stepSimulation(GetFrameTime(), 10);
 
-        m_world->stepSimulation(GetFrameTime(), 10);
-
-        // Remove cubes that fall below -100
-        for (auto it = m_rigidBodies.begin(); it != m_rigidBodies.end(); ) {
-            btTransform transform;
-            (*it)->getMotionState()->getWorldTransform(transform);
-            btVector3 pos = transform.getOrigin();
-
-            if (pos.y() < -100) {
-                m_world->removeRigidBody(*it);
-                delete (*it)->getMotionState();
-                delete (*it)->getCollisionShape();
-                delete *it;
-                it = m_rigidBodies.erase(it);
-                count--;
-            } else {
-                ++it;
-            }
-        }
 
         // Render
         BeginDrawing();
@@ -165,21 +214,21 @@ void Application::run() {
         BeginMode3D(m_camera);
         DrawGrid(100, 1.0f);
 
-        // Draw Ground
-        btTransform groundTransform;
-        m_ground->getMotionState()->getWorldTransform(groundTransform);
-        btVector3 groundPos = groundTransform.getOrigin();
-        DrawCube((Vector3){ groundPos.x(), groundPos.y(), groundPos.z() }, 20, 1, 20, BLUE);
-        DrawCubeWires((Vector3){ groundPos.x(), groundPos.y(), groundPos.z() }, 20, 1, 20, MAROON);
+        DrawModel(m_groundModel, {0, 0, 0}, 1.0f, WHITE);
 
-        for (btRigidBody* body : m_rigidBodies) {
-            drawCube(body);
+        int numCollisionObjects = m_dynamicsWorld->getNumCollisionObjects();
+        for (int i = 0; i < numCollisionObjects; i++) {
+            btCollisionObject* obj = m_dynamicsWorld->getCollisionObjectArray()[i];
+            btRigidBody* body = btRigidBody::upcast(obj);
+            if (body && body->getMotionState()) {
+                drawCube(body);
+            }
         }
 
         EndMode3D();
         DrawText("Bullet Physics with Raylib", 10, 10, 20, BLACK);
         DrawFPS(10, 40);
-        DrawText(TextFormat("Cubes: %d", count), 10, 70, 20, BLACK);
+        DrawText(TextFormat("Cubes: %d", 0), 10, 70, 20, BLACK);
         EndDrawing();
     } 
 
